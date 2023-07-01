0d5f0da67c6373e3b2b5e9484be558256b03f38b
kca2250 <2250katok@gmail.com>
2023-07-02 00:25:46 +0900

    :sparkles: initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [],
+  theme: {
+    colors: {
+      black: "#3f402b",
+      orange: "#f54800",
+      yellow: "#feb516",
+      blue: "#3a59dd",
+      red: "#ff486d",
+      green: "#47b052",
+    },
+    fontSize: {
+      xs: "12px",
+      sm: "14px",
+      base: "16px",
+      lg: "18px",
+      xl: "20px",
+      "2xl": "24px",
+    },
+  },
 };
