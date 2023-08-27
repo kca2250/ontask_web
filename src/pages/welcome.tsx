7c7b40d4b26466aa76b9b793a5e7973b0caa4183
kca2250 <2250katok@gmail.com>
2023-08-27 23:12:42 +0900

    タイトル、アプリ説明、ログインボタンを配置

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,7 +1,41 @@
+import { Box, Container, Heading, Button, Image } from "@chakra-ui/react";
+import Typewriter from "typewriter-effect";
+import { FcGoogle } from "react-icons/fc";
+
 export const WelcomePage: React.FC = (): JSX.Element => {
   return (
-    <>
-      <p>this is welcome page</p>
-    </>
+    <Container
+      minH="100vh"
+      display="flex"
+      justifyContent="center"
+      flexDir="column"
+      gap={9}
+    >
+      <Box display="flex" flexDir="column" gap={2} mt="-15vh">
+        <Box display="flex" alignItems="center" gap={1}>
+          <Image boxSize="30px" objectFit="cover" src="../../public/logo.svg" />
+          <Heading as="h1" size="lg">
+            ontask
+          </Heading>
+        </Box>
+
+        <Typewriter
+          options={{
+            strings: ["Simplify your tasks, clarify your life's direction."],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </Box>
+
+      <Button
+        leftIcon={<FcGoogle style={{ fontSize: "24px" }} />}
+        aria-label="signup-button"
+        variant="solid"
+        w="fit-content"
+      >
+        Login with Google
+      </Button>
+    </Container>
   );
 };
